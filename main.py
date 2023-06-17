634a8f6ba5813e4863ef44b8aca70b43f019e444
Abdnahid <68383656+abdnahid@users.noreply.github.com>
2023-06-17 19:14:32 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, send_file
+from flask_cors import CORS
+from openpyxl import Workbook
 import os
+from test_report import InspectionExcel
 
 app = Flask(__name__)
+cors = CORS(app)
 
 
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def home():
+    return "Hey there! Its working!"
 
 
 if __name__ == '__main__':
